f86cbe940e09aa45aefccb26a5b7047cece864b7
Kristof-ay <kris200036@gmail.com>
2025-03-16 22:59:13 +0100

    server performance so far

---
+++ project/server-stats.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+echo("############")
+echo("CPU")
+
+top -bn2 | grep "Cpu(s)" | \
+           sed "s/.*, *\([0-9.]*\)%* id.*/\1/" | \
+           awk '{print 100 - $1"%"}'
+
+echo("############")
+echo("Total memmory")
+
+free | grep "Mem:" | awk '{print $3 "-" ($3/$2)*100 "%", $4 "-" ($4/$2)*100 "%"}'
+
+echo("#############")
+echo("Total disk")
+
+df -h | awk '{gsub("%", "", $5); print $5, "%", 100-$5 "%"}'
+
+echo("#############")
+echo("5 process by cpu usage")
+
